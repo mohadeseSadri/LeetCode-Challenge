9090ce43d8ee46393c66cb4c578a57fa192a03a5
Mohadese <Sadri.Mohadeseh@Tapsi.doctor>
2026-04-27 15:09:10 +0330

    Given an integer array nums, a decrement function fn, and an initial value init, I returned the final result of executing the function fn on each element of the array in order, passing the return value from the calculation on the previous element.

--- challenge.js
+++ challenge.js
@@ -25,7 +25,6 @@ console.log(array1.last());
 const array2 = [];
 console.log(array2.last());
 
-
 // 3.Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 let arrayItems = ["Ali", "Mary", "Mohadese", "zahra"];
 
@@ -40,3 +39,16 @@ function counter(n) {
   return numbers;
 }
 console.log(counter(10));
+
+// 4.Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+// If the length of the array is 0, the function should return init.
+// Please solve it without using the built-in Array.reduce method.
+
+function reduce(nums, fn, init) {
+  for (let num of nums) {
+    init = fn(init, num);
+  }
+  return init;
+}
+console.log(reduce([1, 2, 3, 4], (i, n) => i + n, 0));
